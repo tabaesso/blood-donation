9fe04e8bfc83a17fe4e637ef186b4b9a34c97fde
tabaesso <tatabaesso@gmail.com>
2020-03-15 18:12:16 -0300

    config server para mostrar arquivos estáticos

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require("express")
 const server = express()
 const SERVER_PORT = 3000;
 
+//config server to show extra static content
+server.use(express.static('public'))
+
+//config template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
+})
+
 server.get("/", function(req, res) {
-    return res.send("eai man?")
+    return res.render("index.html")
 })
 
 server.listen(SERVER_PORT, function() {
