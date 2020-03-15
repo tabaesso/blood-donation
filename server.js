9720b8006c2a1165be4c033fa2bf84c1fc51d9b5
tabaesso <tatabaesso@gmail.com>
2020-03-15 19:31:08 -0300

    adiciona doadores a lista

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const SERVER_PORT = 3000;
 //config server to show extra static content
 server.use(express.static('public'))
 
+//body form
+server.use(express.urlencoded({ extended: true }))
+
 //config template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -33,6 +36,19 @@ const donors = [
 
 server.get("/", function(req, res) {
     return res.render("index.html", { donors })
+})
+
+server.post("/", function(req, res) {
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    donors.push({
+        name: name,
+        blood: blood,
+    })
+
+    return res.redirect("/")
 })
 
 server.listen(SERVER_PORT, function() {
